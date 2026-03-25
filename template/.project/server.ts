e417d92625dee2df9d2d76f6fd3a3033374ad58b
rpostulart <ramonpostulart@gmail.com>
2026-03-25 12:24:59 +0100

    fixces ui location

--- template/.project/server.ts
+++ template/.project/server.ts
@@ -5,7 +5,7 @@
 await loadEnv();
 
 const args = parseArgs(Deno.args);
-const PROJECT_DIR = resolve(args.project || Deno.env.get("PROJECT_DIR") || "./.project");
+const PROJECT_DIR = resolve(args.project || Deno.env.get("PROJECT_DIR") || new URL(".", import.meta.url).pathname);
 const PORT = parseInt(args.port || Deno.env.get("PORT") || "8000");
 const UI_DIR = resolve(new URL(".", import.meta.url).pathname, "ui");
 const CURRENT_USER = Deno.env.get("PROJECT_USER") || "";
